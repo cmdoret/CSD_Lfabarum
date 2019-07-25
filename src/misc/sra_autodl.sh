b027555aecc5c2bc65cee46faf4282e8aefbabf0
cmdoret <cyril.matthey-doret@unil.ch>
2019-07-25 03:25:24 +0200

    add string formatting to loop over SRA records for downloading input data

--- src/misc/sra_autodl.sh
+++ src/misc/sra_autodl.sh
@@ -3,14 +3,17 @@
 # Requires the fasterq-dump from NCBI
 # cmdoret, 20190509
 
-sra_start=$1
-sra_end=$2
-out_dir=$3
+SRA_START=$1
+SRA_END=$2
+OUT_DIR=$3
 
 # Extract longest common prefix between identifiers
-sra_prefix=$(grep -zPo '(.*).*\n\K\1' <<< "$sra_start"$'\n'"$sra_end")
-sra_num_start=${sra_start#????}
-sra_num_end=${sra_end#????}
-for sra_id in ${sra_prefix}{$sra_num_start..$sra_num_end}; do
-        fasterq-dump 
+SRA_PREFIX=$(grep -zPo '(.*).*\n\K\1' <<< "$SRA_START"$'\n'"$SRA_END")
+SRA_NUM_START=${SRA_START#$SRA_PREFIX}
+SRA_NUM_END=${SRA_END#$SRA_PREFIX}
+
+# Iterate over range of variable ids and prepend common prefix
+for ((sra_id=SRA_NUM_START;sra_id<=SRA_NUM_END;sra_id++)); do
+		# Download each fastq
+		fasterq-dump -e 4 "${SRA_PREFIX}"$(printf "%02" "$sra_id") -o $OUT_DIR
 done
